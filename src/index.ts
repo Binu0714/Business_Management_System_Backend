3d588ea4b76e0f28098611a489dd71fe946f3515
Binu Jinajith <binuj714@gmail.com>
2026-06-25 13:20:47 +0530

    chore: export app default for vercel serverless support

--- src/index.ts
+++ src/index.ts
@@ -28,3 +28,5 @@ app.get('/api/health', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server on http://localhost:${PORT}`));
+
+export default app;
